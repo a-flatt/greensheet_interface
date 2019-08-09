3bb268c5edb172760c3bfdb067d7320dd186333f
a-flatt <adamjplatt@outlook.com>
2019-08-09 09:31:57 +0800

    create formatting file

--- greensheet_utils.py
+++ greensheet_utils.py
@@ -6,6 +6,10 @@ def reformat_sheet(worksheet):
 	for row in range(1, worksheet.max_row + 1):
 		worksheet.row_dimensions[row].height = 21.0
 
-def formatter(col="", div="", row=""):
+def formatter(col="", row=""):
 
-	return get_column_letter(col) + div + str(row)
+	return get_column_letter(col) + str(row)
+
+def num_styled_cols(worksheet, tgt_range):
+
+	return len([cell._style for cell in worksheet[tgt_range]])
